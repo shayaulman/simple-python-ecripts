82bf0a256926d0f20a06300a01d55c004156b485
Shaye Ulman <shayeulman@gmail.com>
2019-09-23 22:13:51 +0300

    🛠 fix in code

--- days-to-birthday.py
+++ days-to-birthday.py
@@ -1,14 +1,14 @@
 from datetime import date   
 
 print "Please enter Your birthday (only numbers!):"
-year = input("(example: 1992):")
+year = input("Year of birth (example: 1992):")
 month = input("Month (example: 7):")
 day = input("Day In Month (example: 9):")
 
 today = date.today()
-birthday = date(today.year, month, day)
+birthday = date(today.year +1, month, day)
 days_untill_birthday = birthday - today
 
-print "In " + str(days_untill_birthday.days) + " days you will turn " + str(today.year - year) + ", Mazal Tov!!"
+print "In " + str(days_untill_birthday.days) + " days you will turn " + str(today.year - (year -1)) + ", Mazal Tov!!"
 
 
