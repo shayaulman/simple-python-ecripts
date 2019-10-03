643d8a33fee7cf713ee7359be0aa15eb45be1e32
Shaye Ulman <shayeulman@gmail.com>
2019-10-03 22:12:10 +0300

    updated email script 🕊

--- send-email.py
+++ send-email.py
@@ -1,16 +1,18 @@
+# -*- coding: utf-8 -*-
 import yagmail
 
 sender_email = '0583254409a@gmail.com'
-receiver_email = 'shayeulman@gmail.com'
-subject = "Sent from the terminal!!!"
-sender_password = raw_input('Please, enter the password:')
+receiver_email = 'c0533175399@gmail.com'
+subject = "נשלח עם פייתון!!"
+sender_password = raw_input(' נא הכנס את הסיסמא של' + sender_email)
 
 yag = yagmail.SMTP(user=sender_email, password=sender_password)
 
 contents = [
-  "This E-Mail is sent from  the terminal!",
-  "using a Python library",
-  "called Yagmail, It's so easy, just a few lines of code!!!",
+  "...האימייל הזה נשלח אליך עם פייתון",
+  "ראה את הקוד כאן:",
+  "https://github.com/shayaulman/simple-python-scripts/commit/f86dd0539c89b1d48d89c45edcfe97640973c3b2",
+  "כדי שזה יעבוד יש  צורך להתקין ספריות מסיומות, ככה שזה לא יעבוד לך מיד..."
 ]
 
 yag.send(receiver_email, subject, contents)
